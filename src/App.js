84a364466745c69ddb6271c1bdd986b291921c26
farhanjawad <farhanjawad@yandex.com>
2022-06-20 21:38:12 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react"
-//import "style.css"
+import "./index.css"
 import "tailwindcss/base.css"
-import AnimationRevealPage from "./helpers/AnimationRevealPage"
-import Hero from "./components/hero/FullWidthWithImage"
+//import AnimationRevealPage from "./helpers/AnimationRevealPage"
+import Hero from "./components/hero/TwoColumnWithPrimaryBackground"
 //import Features from "./components/features/ThreeColSimple"
 
 function App() {
   return (
-    <AnimationRevealPage disabled>
       <Hero />
-    </AnimationRevealPage>
   )
 }
 export default App; 

f74e1d5a96d3ff6cef9777371c388ab0a985803c
farhanjawad <farhanjawad@yandex.com>
2022-06-20 21:39:29 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./index.css"
 import "tailwindcss/base.css"
 //import AnimationRevealPage from "./helpers/AnimationRevealPage"
-import Hero from "./components/hero/TwoColumnWithPrimaryBackground"
+import Hero from "./components/hero/FullWidthWithImage"
 //import Features from "./components/features/ThreeColSimple"
 
 function App() {
